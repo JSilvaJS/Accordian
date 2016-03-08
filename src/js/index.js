273f6f911e6fa4be54d32201a17297181535cdef
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-08 17:32:17 -0500

    Stuck on getting section 3 to move up... scss is quite unresponsive.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 // Javascript Entry Point
 import $ from 'jquery';
 
-$('.box_1').on('click', function() {
+$('.box_2').on('click', function() {
 	$('.box_2').toggleClass('slideClass');
+});
+
+$('.box_3').on('click', function() {
+	$('.box_3').toggleClass('slideClass_3');
 });
